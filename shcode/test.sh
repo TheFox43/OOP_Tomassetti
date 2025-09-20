6b3b727c18bc0a9043f3383cc8a3851f50e77761
Francesco Volpe <francesco.volpe1998@gmail.com>
2025-09-20 14:07:48 +0200

    trying things in test.sh

--- shcode/test.sh
+++ shcode/test.sh
@@ -1,1 +1,10 @@
-N= wc -l ../data_shell/molecules/*.pdb | wc -l | tr -d ' '
+#Calcolo numero di file molecules
+path='../data_shell/molecules/'
+N=$(wc -l $path*.pdb | wc -l | tr -d ' ')
+((N--))
+echo $N
+
+#Ordinamento e stampa lista dei file molecule in ordine di grandezza
+wc -l ../data_shell/molecules/*.pdb | sort -n | tr -d ' ' | head -n $N > ./sortedbylines.txt
+cat ./sortedbylines.txt
+echo "Circa sium"
